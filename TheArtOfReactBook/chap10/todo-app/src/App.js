a605d43c5a161910cce2275340ba66048e32ea93
sunmi <dsm851105@gmail.com>
2021-07-01 23:58:20 +0900

    componet 끝

--- TheArtOfReactBook/chap10/todo-app/src/App.js
+++ TheArtOfReactBook/chap10/todo-app/src/App.js
@@ -1,10 +1,15 @@
+import TodoInsert from 'components/TodoInsert';
+import TodoList from 'components/TodoList';
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 
-
-
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자</TodoTemplate>
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  );
 };
 
 export default App;
