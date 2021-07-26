eca07b6bb784a9ad32b6f9914b868cd859deeb22
sunmi <dsm851105@gmail.com>
2021-07-26 17:40:39 +0900

    end chap 19

---
+++ TheArtOfReactBook/chap19/splitting-sample/src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Suspense, useState } from "react";
+import loadable from "@loadable/component";
+
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
+
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          hello world
+        </p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ TheArtOfReactBook/chap19/splitting-sample/src/App_beforReact.lazy.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    SplitMe: null,
+  };
+
+  handleClick = async () => {
+    const loadedModule = await import("./SplitMe");
+    this.setState({
+      SplitMe: loadedModule.default,
+    });
+  };
+  render() {
+    const { SplitMe } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handelClick}>hello world</p>
+          {SplitMe && <SplitMe />}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
